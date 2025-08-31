e4e08f80645e2ecba5427f02b7e3911435959be9
Igor da Silva Dias <artscolha@gmail.com>
2025-08-31 18:07:19 -0300

    next-4-contador V1.0

--- app/page.js
+++ app/page.js
@@ -1,20 +1,19 @@
 "use client"
-import { useState } from 'react'
+import { useState } from "react"
+
 export default function Home() {
   const [contador, setContador] = useState(0)
-
-  const menos = () => setContador(contador - 1);
-  const zero = () => setContador(0);
-  const mais = () => setContador(contador + 1);
+  const menos = () => setContador(contador - 1)
+  const zero = () => setContador(0)
+  const mais = () => setContador(contador + 1)
 
   const light = () => {
-    document.body.style.backgroundColor = 'white';
-    document.body.style.color = 'black'
-
+    document.body.style.backgroundColor = "white"
+    document.body.style.color = "black"
   }
   const dark = () => {
-    document.body.style.backgroundColor = 'black';
-    document.body.style.color = 'white'
+    document.body.style.backgroundColor = "black"
+    document.body.style.color = "white"
   }
   return (
     <div>
@@ -23,7 +22,7 @@ export default function Home() {
         <button onClick={dark}>Dark</button>
       </nav>
       <h1>Contador</h1>
-      <h2>{contador}</h2>
+      <p>{contador}</p>
       <button onClick={menos}>-1</button>
       <button onClick={zero}>Reset</button>
       <button onClick={mais}>+1</button>
